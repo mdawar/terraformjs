2b2c918a1ddc60a05bc485ca4bb810d3f619a17f
Pierre Mdawar <p.mdawar@gmail.com>
2020-01-06 18:58:45 +0200

    [tests] added Interpolation class tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,7 +1,45 @@
 import { Interpolation } from '../api.js';
 
-test('Expression is wrapped in ${}', () => {
-  const expr = new Interpolation('var.foo');
+describe('Interpolation class', () => {
+  test('Expression is wrapped in ${}', () => {
+    const expr = new Interpolation('var.foo');
 
-  expect(String(expr)).toBe('${var.foo}');
+    expect(String(expr)).toBe('${var.foo}');
+  });
+
+  test('Accessing a dynamic property appends the property to the expression', () => {
+    const expr = new Interpolation('aws_instance.www');
+
+    expect(String(expr.id)).toBe('${aws_instance.www.id}');
+  });
+
+  test('Accessing a dynamic property using brackets [] appends the property to the expression', () => {
+    const expr = new Interpolation('aws_instance.www');
+
+    expect(String(expr['id'])).toBe('${aws_instance.www.id}');
+  });
+
+  test('Chaining dynamic properties appends the properties to the expression', () => {
+    const expr = new Interpolation('aws_instance.www');
+
+    expect(String(expr.first.second.third)).toBe('${aws_instance.www.first.second.third}');
+  });
+
+  test('Using a dynamic index appends the index to the expression', () => {
+    const expr = new Interpolation('aws_instance.www');
+
+    expect(String(expr[0])).toBe('${aws_instance.www[0]}');
+  });
+
+  test('Chaining multiple dynamic indexes appends the indexes to the expression', () => {
+    const expr = new Interpolation('aws_instance.www');
+
+    expect(String(expr[0][1][100])).toBe('${aws_instance.www[0][1][100]}');
+  });
+
+  test('Chaining dynamic properties and indexes appends the properties and indexes to the expression', () => {
+    const expr = new Interpolation('aws_instance.www');
+
+    expect(String(expr.first[0].second[1].third[100])).toBe('${aws_instance.www.first[0].second[1].third[100]}');
+  });
 });
